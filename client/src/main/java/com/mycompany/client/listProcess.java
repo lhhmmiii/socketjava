a0b72a8ecfe12fc9be877e8efd41ba625ddbb9ba
takhoa2702 <115924588+takhoa2702@users.noreply.github.com>
2022-11-14 09:32:50 +0700

    Update listProcess.java

--- client/src/main/java/com/mycompany/client/listProcess.java
+++ client/src/main/java/com/mycompany/client/listProcess.java
@@ -112,7 +112,7 @@ public class listProcess extends javax.swing.JFrame {
             String soprocess = program.is.readLine();
             int soprocess1 = 0;
             soprocess1 = Integer.parseInt(soprocess);
-            ObjectInputStream oin = new ObjectInputStream(program.sclient.getInputStream());
+            ObjectInputStream ois = new ObjectInputStream(program.sclient.getInputStream());
             for (int i = 0; i < soprocess1-4; i++)
             {
                 String[] data = (String[]) oin.readObject();
